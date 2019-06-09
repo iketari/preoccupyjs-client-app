d5bfac1205f72a69763f7d82fc6c2dbd7ed1eb12
Artsiom Mezin <iketari@gmail.com>
2019-06-09 17:35:53 +0100

    Updates

---
+++ precache-manifest.efa4d2a4a943e18921f311d15f140925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0951ce572fc92552f527ece4f4c750af",
+    "url": "/preoccupyjs-client-app/index.html"
+  },
+  {
+    "revision": "af55ed00de3b8f2a441d",
+    "url": "/preoccupyjs-client-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "56b741573b706da761c6",
+    "url": "/preoccupyjs-client-app/static/css/main.ffd97e3c.chunk.css"
+  },
+  {
+    "revision": "af55ed00de3b8f2a441d",
+    "url": "/preoccupyjs-client-app/static/js/2.9d979787.chunk.js"
+  },
+  {
+    "revision": "56b741573b706da761c6",
+    "url": "/preoccupyjs-client-app/static/js/main.0cd85406.chunk.js"
+  },
+  {
+    "revision": "9399d7ee6ba74b0a12fc",
+    "url": "/preoccupyjs-client-app/static/js/runtime~main.b7425232.js"
+  }
+]);
